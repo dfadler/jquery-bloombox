602a6412c3693fa582905b54d35a14f1aadd47e9
Dustin <dustin@toky.com>
2011-10-04 09:15:48 -0500

    Working click progression with animation

--- source/javascripts/jquery.bloom-box.js
+++ source/javascripts/jquery.bloom-box.js
@@ -1,221 +1,255 @@
+/* jslint devel: true, white: true, maxerr: 50, indent: 4 */
+
 (function ($) {
 
-	'use strict';
+    'use strict';
 
-	$.fn.bloomBox = function (options) {
+    $.fn.bloomBox = function (options) {
 
-		// Variables
-		var $box = this,
-			opts = $.extend({}, $.fn.bloomBox.defaults, options),
-			articles = opts.articles ? $(opts.articles, $box): $box.children(),
-			activeArticle = articles[0],
-			boxHeight = $box.height(),
-			boxWidth = $box.width();
+        // Variables
+        var $box = this,
+        opts = $.extend({},
+        $.fn.bloomBox.defaults, options),
+        articles = opts.articles ? $(opts.articles, $box) : $box.children(),
+        activeArticle = 0,
+        boxHeight = $box.height(),
+        boxWidth = $box.width();
 
-		// Functions 
+        // Functions
+        function findActiveArticle() {
+            $(articles)
+            .each(function () {
+                if ($(this).hasClass('active')) {
+                    activeArticle = this;
+                }
+            });
 
-		function findActiveArticle() {
-			$(articles)
-				.each(function () { 
-					if ($(this).hasClass('active')) {
-						activeArticle = this;
-					}
-				});
-				
-			return activeArticle;
-		}
+            return activeArticle;
+        }
 
-		function bloomIn(e) {
+        function bloomIn(e) {
 
-			$('.leftClone', findActiveArticle()).animate({
-				'opacity': 1,
-				'width': boxWidth / 2,
-				'height': boxHeight,
-				'marginLeft': -(boxWidth / 4),
-				'marginTop': -(boxHeight / 2)
-			},
-			100, e === 'click' ? 'jswing' : 'easeOutBack', function () {
-				if ($box.hasClass('hovering')) {
-					$('.left', findActiveArticle()).animate({
-						'opacity': 1
-					},
-					50);
-				} else {
-					$('.left', findActiveArticle()).animate({
-						'opacity': 0
-					},
-					50);
-				}
-			});
+            $('.leftClone', findActiveArticle()).animate({
+                'opacity': 1,
+                'width': boxWidth / 2,
+                'height': boxHeight,
+                'marginLeft': -(boxWidth / 4),
+                'marginTop': -(boxHeight / 2)
+            },
+            opts.speed, e === 'click' ? 'jswing': 'easeOutBack',
+            function () {
+                if ($box.hasClass('hovering')) {
+                    $('.left', findActiveArticle()).animate({
+                        'opacity': 1
+                    },
+                    opts.speed / 2);
+                } else {
+                    $('.left', findActiveArticle()).animate({
+                        'opacity': 0
+                    },
+                    opts.speed / 2);
+                }
+            });
 
-			$('.rightClone', findActiveArticle()).animate({
-				'opacity': 1,
-				'width': boxWidth / 2,
-				'height': boxHeight,
-				'marginRight': -(boxWidth / 4),
-				'marginTop': -(boxHeight / 2)
-			},
-			200, e === 'click' ? 'jswing' : 'easeOutBack', function () {
-				if ($box.hasClass('hovering')) {
-					$('.right', findActiveArticle()).animate({
-						'opacity': 1
-					},
-					100);
-				} else {
-					$('.right', $box).animate({
-						'opacity': 0
-					},
-					100);
-				}
-			});
-		}
+            $('.rightClone', findActiveArticle()).animate({
+                'opacity': 1,
+                'width': boxWidth / 2,
+                'height': boxHeight,
+                'marginRight': -(boxWidth / 4),
+                'marginTop': -(boxHeight / 2)
+            },
+            opts.speed * 2, e === 'click' ? 'jswing': 'easeOutBack',
+            function () {
+                if ($box.hasClass('hovering')) {
+                    $('.right', findActiveArticle()).animate({
+                        'opacity': 1
+                    },
+                    opts.speed);
+                } else {
+                    $('.right', $box).animate({
+                        'opacity': 0
+                    },
+                    opts.speed);
+                }
+            });
+        }
 
-		function bloomOut(e) {
-			$('.left').animate({
-				'opacity': 0
-			},
-			100, function () {
-				if ($box.hasClass('hovering')) {
-					$('.leftClone', $box).animate({
-						'opacity': 1,
-						'width': boxWidth / 2,
-						'height': boxHeight,
-						'marginLeft': -(boxWidth / 4),
-						'marginTop': -(boxHeight / 2)
-					},
-					200, e === 'click' ? 'jswing' : 'easeOutBack');
-				} else {
-					$('.leftClone', $box).animate({
-						'opacity': 0,
-						'width': '0',
-						'height': '0',
-						'marginLeft': '0',
-						'marginTop': '0'
-					},
-					200, e === 'click' ? 'jswing' : 'easeInBack');
-				}
-			});
+        function bloomOut(e) {
+            $('.left').animate({
+                'opacity': 0
+            },
+            100,
+            function () {
+                if ($box.hasClass('hovering')) {
+                    $('.leftClone', $box).animate({
+                        'opacity': 1,
+                        'width': boxWidth / 2,
+                        'height': boxHeight,
+                        'marginLeft': -(boxWidth / 4),
+                        'marginTop': -(boxHeight / 2)
+                    },
+                    opts.speed * 2, e === 'click' ? 'jswing': 'easeOutBack');
+                } else {
+                    $('.leftClone', $box).animate({
+                        'opacity': 0,
+                        'width': '0',
+                        'height': '0',
+                        'marginLeft': '0',
+                        'marginTop': '0'
+                    },
+                    opts.speed * 2, e === 'click' ? 'jswing': 'easeInBack');
+                }
+            });
 
-			$('.right').animate({
-				'opacity': 0
-			},
-			100, function () {
-				if ($box.hasClass('hovering')) {
-					$('.rightClone', $box).animate({
-						'opacity': 1,
-						'width': boxWidth / 2,
-						'height': boxHeight,
-						'marginRight': -(boxWidth / 4),
-						'marginTop': -(boxHeight / 2)
-					},
-					100, e === 'click' ? 'jswing' : 'easeOutBack');
-				} else {
-					$('.rightClone', $box).animate({
-						'opacity': 0,
-						'width': '0',
-						'height': '0',
-						'marginRight': '0',
-						'marginTop': '0'
-					},
-					100, e === 'click' ? 'jswing' : 'easeInBack');
-				}
-			});
-		}
+            $('.right').animate({
+                'opacity': 0
+            },
+            100,
+            function () {
+                if ($box.hasClass('hovering')) {
+                    $('.rightClone', $box).animate({
+                        'opacity': 1,
+                        'width': boxWidth / 2,
+                        'height': boxHeight,
+                        'marginRight': -(boxWidth / 4),
+                        'marginTop': -(boxHeight / 2)
+                    },
+                    opts.speed, e === 'click' ? 'jswing': 'easeOutBack');
+                } else {
+                    $('.rightClone', $box).animate({
+                        'opacity': 0,
+                        'width': '0',
+                        'height': '0',
+                        'marginRight': '0',
+                        'marginTop': '0'
+                    },
+                    opts.speed, e === 'click' ? 'jswing': 'easeInBack');
+                }
+            });
+        }
 
-		function bloom(event) {
+        function bloom(event) {
 
-			switch (event) {
-			case 'mouseenter':
-				$box.addClass('hovering');
-				// console.log(event);
-				break;
-			case 'mouseleave':
-				$box.removeClass('hovering');
-				// console.log(event);
-				break;
-			case 'click':
-				if ($(articles[articles.length - 1]).hasClass('active')) {
-					
-					$('.left, .right', findActiveArticle())
-						.css({'opacity': 0})
-						.parent()
-						.removeClass('active');
-						
-					$('.left, .right', articles[0])
-						.css({'opacity': 1})
-						.parent()
-						.addClass('active');
+            switch (event) {
+            case 'mouseenter':
+                $box.addClass('hovering');
+                if ($box.hasClass('hovering')) {
+                    $(':animated').stop(true);
+                    bloomIn(event);
+                } else {
+                    $(':animated').stop(true);
+                    bloomOut(event);
+                }
+                break;
+            case 'mouseleave':
+                $box.removeClass('hovering');
+                if ($box.hasClass('hovering')) {
+                    $(':animated').stop(true);
+                    bloomIn(event);
+                } else {
+                    $(':animated').stop(true);
+                    bloomOut(event);
+                }
+                break;
+            case 'click':
 
-				} else {
-					
-					$('.left, .right', findActiveArticle())
-						.css({'opacity': 0})
-						.parent()
-						.removeClass('active')
-						.next()
-						.addClass('active')
-						.find('.left, .right')
-						.css({'opacity': 1});
-					
-				}
-					
-				break;
-			default:
-				// console.log('default');
-				break;
+                var currentIndex = $(findActiveArticle()).index();
 
-			}
-			
-			if ($box.hasClass('hovering')) {
-				$(':animated').stop(true);
-				bloomIn(event);
-			} else {
-				$(':animated').stop(true);
-				bloomOut(event);
-			}
+                if ($(articles[articles.length - 1]).hasClass('active')) {
 
-		}
+                    currentIndex = 0;
 
-		return this.each(function () {
-			
-			// Setup
-			$(articles)
-				.each(function () {
-					
-					$('.left', this).clone(false).appendTo(this).removeAttr('class').addClass('leftClone').css({
-						'width': '0',
-						'height': '0',
-						'marginLeft': '0',
-						'marginTop': '0'
-					}).children().remove();
+                    $('.left, .right', findActiveArticle())
+                    .stop(true)
+                    .animate({
+                        'opacity': 0
+                    },
+                    opts.speed * 2,
+                    function () {
+                        $(findActiveArticle())
+                        .removeClass('active');
 
-					$('.right', this).clone(false).appendTo(this).removeAttr('class').addClass('rightClone').css({
-						'width': '0',
-						'height': '0',
-						'marginRight': '0',
-						'marginTop': '0'
-					}).children().remove();
+                        $(articles[0])
+                        .addClass('active')
+                        .find('.left, .right')
+                        .animate({
+                            'opacity': 1
+                        },
+                        opts.speed * 2);
+                    });
+                } else {
 
-					$('.left, .right').css({
-						'opacity': 0
-					});
-				}).filter(':eq(0)').addClass('active');
+                    currentIndex += 1;
 
-			// Events
-			$box.bind('mouseenter mouseleave click', function (e) {
-				bloom(e.type);
-			});
+                    $('.left, .right', findActiveArticle())
+                    .stop(true)
+                    .animate({
+                        'opacity': 0
+                    },
+                    opts.speed * 2,
+                    function () {
+                        $(findActiveArticle())
+                        .removeClass('active');
 
-			
-		});	
-	};
-	
-	$.fn.bloomBox.defaults = {
-		articles: '.article',
-		autoplay: false,
-		controls: true,
-		loop: false,
-		speed: 200
-	};
-	
+                        $(articles[currentIndex])
+                        .addClass('active')
+                        .find('.left, .right')
+                        .animate({
+                            'opacity': 1
+                        },
+                        opts.speed * 2);
+                    });
+                }
+
+                break;
+            default:
+                break;
+
+            }
+
+        }
+
+        return this.each(function () {
+
+            // Setup
+            $(articles)
+            .each(function () {
+
+                $('.left', this).clone(false).appendTo(this).removeAttr('class').addClass('leftClone').css({
+                    'width': '0',
+                    'height': '0',
+                    'marginLeft': '0',
+                    'marginTop': '0'
+                }).children().remove();
+
+                $('.right', this).clone(false).appendTo(this).removeAttr('class').addClass('rightClone').css({
+                    'width': '0',
+                    'height': '0',
+                    'marginRight': '0',
+                    'marginTop': '0'
+                }).children().remove();
+
+                $('.left, .right').css({
+                    'opacity': 0
+                });
+            }).filter(':eq(0)').addClass('active');
+
+            // Events
+            $box.bind('mouseenter mouseleave click',
+            function (e) {
+                bloom(e.type);
+            });
+
+
+        });
+    };
+
+    $.fn.bloomBox.defaults = {
+        articles: '.article',
+        autoplay: false,
+        controls: true,
+        loop: false,
+        speed: 100
+    };
+
 })(jQuery);
